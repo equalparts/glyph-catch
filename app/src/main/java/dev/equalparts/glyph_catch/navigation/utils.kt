f2fa6e056b1776d562c506541f2217204bf1a064
equalparts <m@equalparts.dev>
2025-10-06 21:15:24 +0200

    fix(navigation): avoid nav bar desync on search from Pokédex

--- app/src/main/java/dev/equalparts/glyph_catch/navigation/utils.kt
+++ app/src/main/java/dev/equalparts/glyph_catch/navigation/utils.kt
@@ -2,11 +2,19 @@ package dev.equalparts.glyph_catch.navigation
 
 import android.net.Uri
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import dev.equalparts.glyph_catch.data.Pokemon
 
 fun NavController.navigateToPokemon(speciesId: Int) {
     val species = Pokemon[speciesId]
-    navigate(AppScreen.Caught.createRoute(species?.name ?: ""))
+    val searchRoute = AppScreen.Caught.createRoute(species?.name ?: "")
+    navigate(searchRoute) {
+        popUpTo(graph.findStartDestination().id) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
 }
 
 fun NavController.navigateToCaughtDetail(pokemonId: String, fromHome: Boolean = false) {
